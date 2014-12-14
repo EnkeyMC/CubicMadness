0079e736e44c00ce0c0fe3e2edd37ff34b2f9050
EnkeyMC <enkeymc@gmail.com>
2014-12-14 14:07:48 +0100

    Created abstract class Particle

---
+++ src/cubicmadness/particle/Particle.java
@@ -0,0 +1,15 @@
+package cubicmadness.particle;
+
+import cubicmadness.bin.GameObject;
+import cubicmadness.bin.GamePanel;
+
+/**
+ *
+ * @author Martin
+ */
+public abstract class Particle extends GameObject{
+    
+    public Particle(GamePanel gp){
+        super(gp);
+    }
+}
