b719ca895814b27d63e2c6f090b8dc8fe91f81a5
EnkeyMC <enkeymc@gmail.com>
2014-12-14 16:44:39 +0100

    Implemeted constructor and tick method of Particle class

--- src/cubicmadness/particle/Particle.java
+++ src/cubicmadness/particle/Particle.java
@@ -2,6 +2,9 @@ package cubicmadness.particle;
 
 import cubicmadness.bin.GameObject;
 import cubicmadness.bin.GamePanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
 
 /**
  *
@@ -9,7 +12,24 @@ import cubicmadness.bin.GamePanel;
  */
 public abstract class Particle extends GameObject{
     
-    public Particle(GamePanel gp){
+    protected int life;
+    protected Point center;
+    
+    public Particle(GamePanel gp, int life, Color c, Point center, float x, float y, Dimension minSize, Dimension maxSize){
         super(gp);
+        this.life = life;
+        this.color = c;
+        this.center = center;
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public void tick(){
+        super.tick();
+        this.life--;
+        if(life <= 0){
+            // remove
+        }
     }
 }
